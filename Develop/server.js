918a8585cf2b15a6623f261bd5ee8b09a8946cde
Ceyda Hergul <ceyda_hergul@hotmail.com>
2021-05-22 00:35:21 +0900

    adding loop

--- Develop/server.js
+++ Develop/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 //for static files
-server.use(express.static(path.join(__dirname, "public"))); 
+server.use(express.static(path.join(__dirname, "./public"))); 
 
 // Basic route that sends the user first to the AJAX Page
 
@@ -26,60 +26,52 @@ server.get("/", (req, res) => res.sendFile(path.join(__dirname, '/public/index.h
 server.get("/notes", (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 server.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')));
 
-//Catch all
-server.get("*", (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 
 //Push notes to the Server
 
-server.post('/api/notes', (req, res) => {
+server.post('/api/notes', function(req, res) {
+    const newNote = req.body;
 
-    fs.readFile(path.join(__dirname, '/db/db.json'), "utf8", (err, resp) => {
-        if (err) {
-            console.log(err);
-        }
-        
-        const notes = JSON.parse(resp);
+    //reads db.json file, parses information and pushes notes variable into the file.
+    fs.readFile('db/db.json', (err, data) => {
+      if (err) throw err;
+      
+      let myNotes = JSON.parse(data);
+      myNotes.push(newNote);
 
-        //url body
-        const myNotes = req.body;
+      myNotes.forEach((note, index)=>{
+        note.id= 1
+        note.id++;
+        return myNotes;
+      })
+      res.json(newNote);
+     
+      stringMyData = JSON.stringify(myNotes);
 
-        const newNote = {
-            title: myNotes.title,
-            text: myNotes.text,
-        };
-
-        notes.push(newNote);
-        res.json(newNote);
-
-        //Need write file method to push notes on the app 
-
-        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(database), (err) => {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("Your note saved!");
-        });
-        
+      fs.writeFile('db/db.json', stringMyData, (err, data) => {
+        if (err) throw err;
+      });
     });
-});
+    
+  });
 
 
 
 //See the notes on the app
 
-server.get("/api/notes/:title", (req, res) => {
-    const selectNotes = req.params.title;
-    res.json(selectNotes);
-});
+server.get('/api/notes', function(req, res) {
+    fs.readFile('db/db.json', (err, data) => {
+      if (err) {
+      throw err;}
+
+      JSON.parse(data);
+      res.send(data);
+    });
+  });
 
 //Delete notes
-server.delete("/api/notes/:title"), (req, res) => {
-    const deleteTitle= req.param.title;
-    res.json(deleteTitle);
 
-
-};
 
 
 
